ec82705e3c32a2d6302fcca56d4833b6644f8da8
micromys <micromys@users.noreply.github.com>
2026-06-13 07:45:01 +0200

    Update access_token.sh

--- access_token.sh
+++ access_token.sh
@@ -1,6 +1,6 @@
 set -x
 user='<UserName>'
 password='<Password>'
-envoy_serial='<Envoy_SerilaNo>'
+envoy_serial='<Envoy_SerialNo>'
 session_id=$(curl -X POST https://enlighten.enphaseenergy.com/login/login.json? -F "user[email]=$user" -F "user[password]=$password" | jq -r ".session_id")
 web_token=$(curl -X POST https://entrez.enphaseenergy.com/tokens -H "Content-Type: application/json" -d "{\"session_id\": \"$session_id\", \"serial_num\": \"$envoy_serial\", \"username\": \"$user\"}")
